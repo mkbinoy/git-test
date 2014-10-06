542c5b9eafd7231f43b916957d7d4bd86deb81c2
bin4cob <bin4cob@COB1004284.cob.apac.bosch.com>
2014-10-07 00:15:36 +0200

    New feature

--- src/Test.java
+++ src/Test.java
@@ -4,6 +4,13 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
                 //commit from remote
+		sayHello();
 	} 
 
+	/**
+	 * Feature
+	 */
+	public static void sayHello(){
+		System.out.println("Hello");
+	}
 }
